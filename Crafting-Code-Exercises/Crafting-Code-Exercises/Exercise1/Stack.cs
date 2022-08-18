b0c3c551575e03d5f0bbfb1bbaebc8936fe35cb4
Daniel Russom <daniel.russom00@gmail.com>
2022-08-18 16:14:52 +0100

    Ex1: Single pop

---
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Exercise1/Stack.cs
@@ -0,0 +1,17 @@
+﻿namespace Crafting_Code_Exercises.Exercise1
+{
+    internal class Stack
+    {
+        object HeldObject;
+
+        internal object Pop()
+        {
+            return HeldObject;
+        }
+
+        internal void Push(object input)
+        {
+            HeldObject = input;
+        }
+    }
+}
---
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Exercise1/StackShould.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Crafting_Code_Exercises.Exercise1
+{
+    [TestClass]
+    public class StackShould
+    {
+        [TestMethod]
+        public void Pop_the_last_pushed_object()
+        {
+            var UnderTest = new Stack();
+            var input = new object();
+
+            UnderTest.Push(input);
+
+            var result = UnderTest.Pop();
+
+            Assert.AreEqual(input, result);
+        }
+    }
+}
