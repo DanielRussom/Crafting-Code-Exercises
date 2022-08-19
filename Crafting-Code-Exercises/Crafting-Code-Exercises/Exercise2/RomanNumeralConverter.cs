07492ecbd3343d66ef725b4a6d43c440a771692c
Daniel Russom <daniel.russom00@gmail.com>
2022-08-19 09:23:24 +0100

    Init Ex2 + Convert to I

---
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Exercise2/RomanNumeralConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace Crafting_Code_Exercises.Exercise2
+{
+    internal class RomanNumeralConverter
+    {
+        internal object Convert(int input)
+        {
+            return "I";
+        }
+    }
+}
---
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Exercise2/RomanNumeralConverterShould.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Crafting_Code_Exercises.Exercise2
+{
+    [TestClass]
+    public class RomanNumeralConverterShould
+    {
+        [TestMethod]
+        public void Convert_1_to_I()
+        {
+            var UnderTest = new RomanNumeralConverter();
+
+            var result = UnderTest.Convert(1);
+
+            Assert.AreEqual("I", result);
+        }
+    }
+}
