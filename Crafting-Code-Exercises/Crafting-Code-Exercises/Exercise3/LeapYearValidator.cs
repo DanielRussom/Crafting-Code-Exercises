fcaf7489605e49962606424b07afcdd8f3c21a90
Matthew King <matthew.king@codurance.com>
2022-08-22 12:55:06 +0100

    Leap Year Validator with Matt

---
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Exercise3/LeapYearValidator.cs
@@ -0,0 +1,17 @@
+﻿namespace Crafting_Code_Exercises.Exercise3
+{
+    public static class LeapYearValidator
+    {
+        public static bool Validate(int year)
+        {
+            if (year.IsDivisibleBy(400)) return true;
+            if (year.IsDivisibleBy(100)) return false;
+            return year.IsDivisibleBy(4);
+        }
+
+        public static bool IsDivisibleBy(this int yearToTest, int divisor)
+        {
+            return yearToTest % divisor == 0;
+        }
+    }
+}
---
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Exercise3/LeapYearValidatorShould.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Crafting_Code_Exercises.Exercise3
+{
+    [TestClass]
+    public class LeapYearValidatorShould
+    {
+        [TestMethod]
+        [DataRow(1999, DisplayName = "1999")]
+        [DataRow(1998, DisplayName = "1998")]
+        [DataRow(1997, DisplayName = "1997")]
+        [DataRow(1700, DisplayName = "1700")]
+        [DataRow(1800, DisplayName = "1800")]
+        [DataRow(1900, DisplayName = "1900")]
+        public void SayYearToTestIsNotALeapYear(int yearToTest)
+        {
+            Assert.IsFalse(LeapYearValidator.Validate(yearToTest));
+        }
+        
+        [TestMethod]
+        [DataRow(1996, DisplayName = "1996")]
+        [DataRow(1992, DisplayName = "1992")]
+        [DataRow(1988, DisplayName = "1988")]
+        [DataRow(1600, DisplayName = "1600")]
+        [DataRow(2000, DisplayName = "2000")]
+        [DataRow(2400, DisplayName = "2400")]
+        public void SayYearToTestIsALeapYear(int yearToTest)
+        {
+            Assert.IsTrue(LeapYearValidator.Validate(yearToTest));
+        }
+    }
+}
