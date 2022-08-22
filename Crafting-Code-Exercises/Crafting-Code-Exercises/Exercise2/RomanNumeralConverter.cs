6f59750ef1a8a6bd1890a3f765afe3a14df3bd30
Daniel Russom <daniel.russom00@gmail.com>
2022-08-22 11:41:09 +0100

    RN Pair programming : Example tests pass

--- Crafting-Code-Exercises/Crafting-Code-Exercises/Exercise2/RomanNumeralConverter.cs
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Exercise2/RomanNumeralConverter.cs
@@ -7,6 +7,14 @@ namespace Crafting_Code_Exercises.Exercise2
     {
         public enum RomanNumerals
         {
+            M = 1000,
+            CM = 900,
+            D = 500,
+            CD = 400,
+            C = 100,
+            XC = 90,
+            L = 50,
+            XL = 40,
             X = 10,
             IX = 9,
             V = 5,
--- Crafting-Code-Exercises/Crafting-Code-Exercises/Exercise2/RomanNumeralConverterShould.cs
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Exercise2/RomanNumeralConverterShould.cs
@@ -20,7 +20,29 @@ namespace Crafting_Code_Exercises.Exercise2
         [DataRow(12, "XII", DisplayName = "12 to XII")]
         [DataRow(13, "XIII", DisplayName = "13 to XIII")]
         [DataRow(14, "XIV", DisplayName = "14 to XIV")]
+        [DataRow(15, "XV", DisplayName = "15 to XV")]
+        [DataRow(40, "XL", DisplayName = "40 to XL")]
+        [DataRow(50, "L", DisplayName = "50 to L")]
+        [DataRow(57, "LVII", DisplayName = "57 to LVII")]
+        [DataRow(90, "XC", DisplayName = "90 to XC")]
+        [DataRow(100, "C", DisplayName = "100 to C")]
+        [DataRow(400, "CD", DisplayName = "400 to CD")]
+        [DataRow(500, "D", DisplayName = "500 to D")]
+        [DataRow(900, "CM", DisplayName = "900 to CM")]
+        [DataRow(1000, "M", DisplayName = "1000 to M")]
         public void Convert(int input, string expected)
+        {
+            Assert.AreEqual(expected, RomanNumeralConverter.Convert(input));
+        }
+
+        [TestMethod]
+        [DataRow(4, "IV", DisplayName = "4 to IV")]
+        [DataRow(9, "IX", DisplayName = "9 to IX")]
+        [DataRow(29, "XXIX", DisplayName = "29 to XXIX")]
+        [DataRow(80, "LXXX", DisplayName = "80 to LXXX")]
+        [DataRow(294, "CCXCIV", DisplayName = "294 to CCXCIV")]
+        [DataRow(2019, "MMXIX", DisplayName = "2019 to MMXIX")]
+        public void PassExampleTests(int input, string expected)
         {
             Assert.AreEqual(expected, RomanNumeralConverter.Convert(input));
         }
