99fc4d1fdb5ecf7e78e5fe3381de69989da747c1
Matthew King <matthew.king@codurance.com>
2022-08-24 16:09:13 +0100

    Implementing Anagram Generator with Matt - yay!

---
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Anagrams/AnagramGenerator.cs
@@ -0,0 +1,37 @@
+﻿namespace Crafting_Code_Exercises.Anagrams
+{
+    public class AnagramGenerator
+    {
+        public List<string> Generate(string input)
+        {
+            if (input.Length == 1) return new List<string> { input };
+
+            if (input.Length == 2)
+            {
+                var reversedString = new string(input.Reverse().ToArray());
+                return new List<string> { input, reversedString };
+            }
+
+            var anagrams = new List<string>();
+
+            for (var index = 0; index < input.Length; index++)
+            {
+                var baseCharacter = input.Substring(index, 1);
+
+                var prefix = input.Substring(0, index);
+                var suffix = input.Substring(index + 1);
+                var newInput = prefix + suffix;
+
+                var tailAnagrams = Generate(newInput);
+
+                foreach (var tailAnagram in tailAnagrams)
+                {
+                    var anagram = baseCharacter + tailAnagram;
+                    anagrams.Add(anagram);
+                }
+            }
+
+            return anagrams;
+        }
+    }
+}
---
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Anagrams/AnagramGeneratorShould.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Crafting_Code_Exercises.Anagrams
+{
+    [TestClass]
+    public class AnagramGeneratorShould
+    {
+        [TestMethod]
+        [DataRow("A", "A", DisplayName = "Get all anagrams of letter A")]
+        [DataRow("B", "B", DisplayName = "Get all anagrams of letter B")]
+        [DataRow("C", "C", DisplayName = "Get all anagrams of letter C")]
+        [DataRow("AB", "AB", "BA", DisplayName = "Get all anagrams of letters AB")]
+        [DataRow("BA", "AB", "BA", DisplayName = "Get all anagrams of letters BA")]
+        [DataRow("CD", "CD", "DC", DisplayName = "Get all anagrams of letters CD")]
+        [DataRow("EF", "EF", "FE", DisplayName = "Get all anagrams of letters EF")]
+        [DataRow("ABC", "ABC", "ACB", "BCA", "BAC",  "CAB", "CBA", DisplayName = "Get all anagrams of letters ABC")]
+        [DataRow("ACB", "ABC", "ACB", "BCA", "BAC",  "CAB", "CBA", DisplayName = "Get all anagrams of letters ACB")]
+        [DataRow("BCA", "ABC", "ACB", "BCA", "BAC",  "CAB", "CBA", DisplayName = "Get all anagrams of letters BCA")]
+        [DataRow("DEF", "DEF", "DFE", "EFD", "EDF",  "FDE", "FED", DisplayName = "Get all anagrams of letters DEF")]
+        [DataRow("biro", "biro", "bior", "brio", "broi", "boir", "bori", "ibro", "ibor", "irbo", "irob", "iobr", "iorb", "rbio", "rboi", "ribo", "riob", "roib", "robi", "obir", "obri", "oibr", "oirb", "orbi", "orib", DisplayName = "Get all anagrams of letters biro")]
+        public void Get_all_anagrams_of_letters(string input, params string[] expectedResult)
+        {
+            var generator = new AnagramGenerator();
+
+            var result = generator.Generate(input);
+
+            CollectionAssert.AreEquivalent(expectedResult.ToList(), result, string.Join(", ", result));
+        }
+    }
+}
