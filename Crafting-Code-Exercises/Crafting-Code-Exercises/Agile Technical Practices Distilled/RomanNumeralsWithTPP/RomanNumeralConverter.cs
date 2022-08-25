690570f8e1cac9b86b5b5d4081dfd82530f50ca5
Matthew King <matthew.king@codurance.com>
2022-08-25 10:30:48 +0100

    Re-organising folder structure.

---
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Agile Technical Practices Distilled/RomanNumeralsWithTPP/RomanNumeralConverter.cs
@@ -0,0 +1,11 @@
+﻿namespace Crafting_Code_Exercises.Agile_Technical_Practices_Distilled.RomanNumeralsWithTPP
+{
+    public class RomanNumeralConverter
+    {
+        public string Convert(int input)
+        {
+            if (input == 2) return "II";
+            return "I";
+        }
+    }
+}
---
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Agile Technical Practices Distilled/RomanNumeralsWithTPP/RomanNumeralsConverterShould.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Crafting_Code_Exercises.Agile_Technical_Practices_Distilled.RomanNumeralsWithTPP;
+
+[TestClass]
+public class RomanNumeralsConverterShould
+{
+    [TestMethod]
+    public void Convert_1_to_I()
+    {
+        var romanNumeralConverter = new RomanNumeralConverter();
+
+        var result = romanNumeralConverter.Convert(1);
+
+        Assert.AreEqual("I", result);
+    }
+
+    [TestMethod]
+    public void Convert_2_to_II()
+    {
+        var romanNumeralConverter = new RomanNumeralConverter();
+
+        var result = romanNumeralConverter.Convert(2);
+
+        Assert.AreEqual("II", result);
+    }
+}
