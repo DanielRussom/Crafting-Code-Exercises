f56ed64fc365c0ec18f9380de82bd9102946393a
Matthew King <matthew.king@codurance.com>
2022-08-31 11:53:02 +0100

    Working on lesson learned during roman numerals kata.
    Previously we missed a rule of three refactor.

--- Crafting-Code-Exercises/Crafting-Code-Exercises/Agile Technical Practices Distilled/RomanNumeralsWithTPP/RomanNumeralConverterWindBack.cs
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Agile Technical Practices Distilled/RomanNumeralsWithTPP/RomanNumeralConverterWindBack.cs
@@ -5,8 +5,26 @@
         // Matthew King 26/08/22 - Non windback approach took quite a long time, see if this approach is any quicker.
         public string Convert(int input)
         {
-            var numerals = new List<string> { "I", "II", "III", };
-            return numerals[input - 1];
+            var result = string.Empty;
+
+            while (input >= 10)
+            {
+                result += "X";
+                input -= 10;
+            }
+
+            if (input >= 5)
+            {
+                result += "V";
+                input -= 5;
+            }
+          
+            for (var index = 1; index <= input; index++)
+            {
+                result += "I";
+            }
+
+            return result;
         }
     }
 }
--- Crafting-Code-Exercises/Crafting-Code-Exercises/Agile Technical Practices Distilled/RomanNumeralsWithTPP/RomanNumeralsConverterWindBackShould.cs
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Agile Technical Practices Distilled/RomanNumeralsWithTPP/RomanNumeralsConverterWindBackShould.cs
@@ -9,9 +9,20 @@ public class RomanNumeralsConverterWindBackShould
     [DataRow(1, "I", DisplayName = "Convert 1 to I")]
     [DataRow(2, "II", DisplayName = "Convert 2 to II")]
     [DataRow(3, "III", DisplayName = "Convert 3 to III")]
+    [DataRow(5, "V", DisplayName = "Convert 5 to V")]
+    [DataRow(6, "VI", DisplayName = "Convert 6 to VI")]
+    [DataRow(7, "VII", DisplayName = "Convert 7 to VII")]
+    [DataRow(8, "VIII", DisplayName = "Convert 8 to VIII")]
+    [DataRow(10, "X", DisplayName = "Convert 10 to X")]
+    [DataRow(11, "XI", DisplayName = "Convert 11 to XI")]
+    [DataRow(12, "XII", DisplayName = "Convert 12 to XII")]
+    [DataRow(13, "XIII", DisplayName = "Convert 13 to XIII")]
+    [DataRow(15, "XV", DisplayName = "Convert 15 to XV")]
+    [DataRow(16, "XVI", DisplayName = "Convert 16 to XVI")]
+    [DataRow(20, "XX", DisplayName = "Convert 20 to XX")]
     public void Convert_decimal_to_RomanNumeral(int input, string expected)
     {
-        var romanNumeralConverter = new RomanNumeralConverter();
+        var romanNumeralConverter = new RomanNumeralConverterWindBack();
 
         var result = romanNumeralConverter.Convert(input);
 
