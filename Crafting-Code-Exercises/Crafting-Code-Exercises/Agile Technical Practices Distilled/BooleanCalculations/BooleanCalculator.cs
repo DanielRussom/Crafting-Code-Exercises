00210c3e5efbecdb85cc77309996be2d187de9ac
Daniel Russom <daniel.russom00@gmail.com>
2022-08-31 16:31:35 +0100

    Boolean calculator (before parentheses)

---
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Agile Technical Practices Distilled/BooleanCalculations/BooleanCalculator.cs
@@ -0,0 +1,44 @@
+﻿namespace Crafting_Code_Exercises.Agile_Technical_Practices_Distilled.BooleanCalculations
+{
+    internal class BooleanCalculator
+    {
+        internal static bool GetBoolean(string input)
+        {
+            if (input == "NOT TRUE")
+            {
+                return false;
+            }
+
+            if (input == "NOT FALSE")
+            {
+                return true;
+            }
+
+            var splitOrInput = input.Split(" OR ");
+            if (splitOrInput.Length > 1)
+            {
+                var result = false;
+                foreach (var operand in splitOrInput)
+                {
+                    result |= GetBoolean(operand);
+                }
+
+                return result;
+            }
+
+            var splitAndInput = input.Split(" AND ");
+            if (splitAndInput.Length > 1)
+            {
+                var result = true;
+                foreach (var operand in splitAndInput)
+                {
+                    result &= GetBoolean(operand);
+                }
+
+                return result;
+            }
+
+            return Boolean.Parse(input);
+        }
+    }
+}
---
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Agile Technical Practices Distilled/BooleanCalculations/BooleanCalculatorShould.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Crafting_Code_Exercises.Agile_Technical_Practices_Distilled.BooleanCalculations
+{
+    [TestClass]
+    public class BooleanCalculatorShould
+    {
+        [TestMethod]
+        [DataRow("TRUE", true)]
+        [DataRow("FALSE", false)]
+        [DataRow("NOT TRUE", false)]
+        [DataRow("NOT FALSE", true)]
+        [DataRow("TRUE AND FALSE", false)]
+        [DataRow("TRUE AND TRUE", true)]
+        [DataRow("FALSE AND TRUE", false)]
+        [DataRow("FALSE AND FALSE", false)]
+        [DataRow("TRUE OR FALSE", true)]
+        [DataRow("FALSE OR TRUE", true)]
+        [DataRow("FALSE OR FALSE", false)]
+        [DataRow("TRUE OR TRUE", true)]
+        [DataRow("NOT FALSE AND TRUE", true)]
+        [DataRow("NOT TRUE AND TRUE", false)]
+        [DataRow("FALSE AND NOT TRUE", false)]
+        [DataRow("FALSE AND NOT FALSE", false)]
+        [DataRow("NOT FALSE AND NOT TRUE", false)]
+        [DataRow("NOT FALSE AND NOT FALSE", true)]
+        [DataRow("NOT TRUE OR FALSE", false)]
+        [DataRow("TRUE OR NOT FALSE", true)]
+        [DataRow("NOT TRUE OR NOT FALSE", true)]
+        [DataRow("NOT TRUE OR NOT TRUE", false)]
+        [DataRow("TRUE AND TRUE AND TRUE", true)]
+        [DataRow("TRUE AND TRUE AND FALSE", false)]
+        [DataRow("NOT TRUE AND TRUE AND FALSE", false)]
+        [DataRow("NOT TRUE AND NOT TRUE AND NOT FALSE", false)]
+        [DataRow("TRUE AND TRUE AND TRUE AND FALSE", false)]
+        [DataRow("TRUE OR TRUE OR TRUE", true)]
+        [DataRow("FALSE OR TRUE OR TRUE", true)]
+        [DataRow("FALSE OR FALSE OR FALSE", false)]
+        [DataRow("FALSE OR FALSE OR TRUE", true)]
+        [DataRow("FALSE OR FALSE OR FALSE OR TRUE", true)]
+        [DataRow("NOT FALSE OR FALSE OR FALSE OR FALSE", true)]
+        [DataRow("NOT TRUE OR NOT TRUE OR NOT TRUE OR NOT TRUE", false)]
+        [DataRow("TRUE OR TRUE OR TRUE AND FALSE", true)]
+        [DataRow("TRUE AND FALSE OR TRUE OR TRUE AND FALSE", true)]
+        [DataRow("TRUE OR FALSE AND NOT FALSE", true)]
+        public void Return_expected_boolean(string input, bool expected)
+        {
+            var result = BooleanCalculator.GetBoolean(input);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
