7342c9655dd2c9bee975bd69a03d3bf7f4d0ab1a
Daniel Russom <daniel.russom00@gmail.com>
2022-08-31 12:49:52 +0100

    Prime factors to 15

---
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Agile Technical Practices Distilled/PrimeFactorCalculator/PrimeFactors.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+namespace Crafting_Code_Exercises.Agile_Technical_Practices_Distilled.PrimeFactorCalculator
+{
+    internal class PrimeFactors
+    {
+        internal static List<int> GetPrimeNumbers(int input)
+        {
+            var factors = new Dictionary<int, List<int>> {
+                {15, new List<int> { 3, 5 } },
+                {12, new List<int> { 2, 2, 3 } },
+                {9, new List<int> { 3, 3 } },
+                {6, new List<int> { 2, 3 } },
+                {4, new List<int> { 2, 2 } },
+                {3, new List<int> { 3 } },
+                {2, new List<int> { 2 } },
+            };
+
+            return factors[input];
+        }
+    }
+}
---
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Agile Technical Practices Distilled/PrimeFactorCalculator/PrimeFactorsShould.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Crafting_Code_Exercises.Agile_Technical_Practices_Distilled.PrimeFactorCalculator
+{
+    [TestClass]
+    public class PrimeFactorsShould
+    {
+        [TestMethod]
+        [DataRow(2, new int[] { 2 })]
+        [DataRow(3, new int[] { 3 })]
+        [DataRow(4, new int[] { 2, 2 })]
+        [DataRow(6, new int[] { 2, 3 })]
+        [DataRow(9, new int[] { 3, 3 })]
+        [DataRow(12, new int[] { 2, 2, 3 })]
+        [DataRow(15, new int[] { 3, 5 })]
+        public void Get_Prime_Numbers(int input, int[] expected)
+        {
+            CollectionAssert.AreEqual(expected.ToList(), PrimeFactors.GetPrimeNumbers(input));
+        }
+    }
+}
