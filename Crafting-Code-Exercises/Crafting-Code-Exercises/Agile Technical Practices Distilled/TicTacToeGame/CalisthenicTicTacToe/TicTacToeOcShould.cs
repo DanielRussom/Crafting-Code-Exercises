165769554d8ea2c5377cbc89327b17286dbf3445
Daniel Russom <daniel.russom00@gmail.com>
2022-09-07 16:09:45 +0100

    TTTC: Vertical columns

--- Crafting-Code-Exercises/Crafting-Code-Exercises/Agile Technical Practices Distilled/TicTacToeGame/CalisthenicTicTacToe/TicTacToeOcShould.cs
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Agile Technical Practices Distilled/TicTacToeGame/CalisthenicTicTacToe/TicTacToeOcShould.cs
@@ -285,5 +285,88 @@ namespace Crafting_Code_Exercises.Agile_Technical_Practices_Distilled.TicTacToeG
 
             Assert.IsTrue(underTest.IsPlayerWinner(playerX));
         }
+
+        [TestMethod]
+        public void Report_X_as_the_winner_when_they_have_3_in_a_vertical_column_in_column_1()
+        {
+            var underTest = new TicTacToeObjectCalisthenics();
+            var playerX = new Player("X");
+            var playerO = new Player("O");
+
+            underTest.PlaceCounter(new Move(playerX, new Coordinate(1, 0)));
+            underTest.PlaceCounter(new Move(playerO, new Coordinate(2, 2)));
+            underTest.PlaceCounter(new Move(playerX, new Coordinate(1, 1)));
+            underTest.PlaceCounter(new Move(playerO, new Coordinate(2, 1)));
+            underTest.PlaceCounter(new Move(playerX, new Coordinate(1, 2)));
+
+            Assert.IsTrue(underTest.IsPlayerWinner(playerX));
+        }
+
+        [TestMethod]
+        public void Report_X_as_the_winner_when_they_have_3_in_a_vertical_column_in_column_2()
+        {
+            var underTest = new TicTacToeObjectCalisthenics();
+            var playerX = new Player("X");
+            var playerO = new Player("O");
+
+            underTest.PlaceCounter(new Move(playerX, new Coordinate(2, 0)));
+            underTest.PlaceCounter(new Move(playerO, new Coordinate(0, 2)));
+            underTest.PlaceCounter(new Move(playerX, new Coordinate(2, 1)));
+            underTest.PlaceCounter(new Move(playerO, new Coordinate(0, 1)));
+            underTest.PlaceCounter(new Move(playerX, new Coordinate(2, 2)));
+
+            Assert.IsTrue(underTest.IsPlayerWinner(playerX));
+        }
+
+        [TestMethod]
+        public void Report_O_as_the_winner_when_they_have_3_in_a_vertical_column_on_column_0()
+        {
+            var underTest = new TicTacToeObjectCalisthenics();
+            var playerO = new Player("O");
+            var playerX = new Player("X");
+
+            underTest.PlaceCounter(new Move(playerX, new Coordinate(2, 2)));
+            underTest.PlaceCounter(new Move(playerO, new Coordinate(0, 0)));
+            underTest.PlaceCounter(new Move(playerX, new Coordinate(2, 1)));
+            underTest.PlaceCounter(new Move(playerO, new Coordinate(0, 1)));
+            underTest.PlaceCounter(new Move(playerX, new Coordinate(1, 1)));
+            underTest.PlaceCounter(new Move(playerO, new Coordinate(0, 2)));
+
+            Assert.IsTrue(underTest.IsPlayerWinner(playerO));
+        }
+
+        [TestMethod]
+        public void Report_O_as_the_winner_when_they_have_3_in_a_vertical_column_on_column_1()
+        {
+            var underTest = new TicTacToeObjectCalisthenics();
+            var playerO = new Player("O");
+            var playerX = new Player("X");
+
+            underTest.PlaceCounter(new Move(playerX, new Coordinate(2, 2)));
+            underTest.PlaceCounter(new Move(playerO, new Coordinate(1, 0)));
+            underTest.PlaceCounter(new Move(playerX, new Coordinate(0, 0)));
+            underTest.PlaceCounter(new Move(playerO, new Coordinate(1, 1)));
+            underTest.PlaceCounter(new Move(playerX, new Coordinate(0, 1)));
+            underTest.PlaceCounter(new Move(playerO, new Coordinate(1, 2)));
+
+            Assert.IsTrue(underTest.IsPlayerWinner(playerO));
+        }
+
+        [TestMethod]
+        public void Report_O_as_the_winner_when_they_have_3_in_a_vertical_column_on_column_2()
+        {
+            var underTest = new TicTacToeObjectCalisthenics();
+            var playerO = new Player("O");
+            var playerX = new Player("X");
+
+            underTest.PlaceCounter(new Move(playerX, new Coordinate(1, 0)));
+            underTest.PlaceCounter(new Move(playerO, new Coordinate(2, 0)));
+            underTest.PlaceCounter(new Move(playerX, new Coordinate(0, 0)));
+            underTest.PlaceCounter(new Move(playerO, new Coordinate(2, 1)));
+            underTest.PlaceCounter(new Move(playerX, new Coordinate(1, 1)));
+            underTest.PlaceCounter(new Move(playerO, new Coordinate(2, 2)));
+
+            Assert.IsTrue(underTest.IsPlayerWinner(playerO));
+        }
     }
 }
