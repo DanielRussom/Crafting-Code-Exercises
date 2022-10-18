d27b1664edc113f8751d8d1400a84c55f4ef60b7
Matthew King <matthew.king@codurance.com>
2022-10-18 14:46:04 +0100

    Refactored some shit

--- Crafting-Code-Exercises/Crafting-Code-Exercises/Agile Technical Practices Distilled/GameofLife/GameOfLifeEngine.cs
+++ Crafting-Code-Exercises/Crafting-Code-Exercises/Agile Technical Practices Distilled/GameofLife/GameOfLifeEngine.cs
@@ -20,31 +20,46 @@
 
             for (var rowLoopCounter = 0; rowLoopCounter <= 2; rowLoopCounter++)
             {
-                var newCellsRow = new List<Cell>();
-                var newCellState = CellState.Dead;
-
-                for (var columnLoopCounter = 0; columnLoopCounter <= 2; columnLoopCounter++)
-                {
-                    var numberOfLiveNeighboursForCell = _board.GetNumberOfLiveNeighboursForCell(new CellPosition(rowLoopCounter, columnLoopCounter));
-                    if (numberOfLiveNeighboursForCell.GetPopulationState() == PopulationState.UnderPopulated)
-                    {
-                        newCellState = CellState.Dead;
-                    }
-
-                    if (numberOfLiveNeighboursForCell.GetPopulationState() == PopulationState.PerfectlyPopulated)
-                    {
-                        var currentLeftCentreCellState = _board.Rows[rowLoopCounter].Cells[columnLoopCounter].State; // Matt King 14/10/2022 - This is a temporary workaround to enable us to continue to defer moving to using a loop.  Remove this!!
-                        newCellState = currentLeftCentreCellState;
-                    }
-
-                    newCellsRow.Add(new Cell(newCellState));
-                }
-
+                var newCellsRow = CreateNewRow(new CellPosition(rowLoopCounter, -1));
                 newRows.Add(new(newCellsRow));
             }
 
             var newBoard = new Board(newRows);
             _board = newBoard;
+        }
+
+        private List<Cell> CreateNewRow(CellPosition cellPosition)
+        {
+            var newCellsRow = new List<Cell>();
+
+            for (var columnLoopCounter = 0; columnLoopCounter <= 2; columnLoopCounter++)
+            {
+                var newCellState = CreateNewCell(new CellPosition(cellPosition.Row, columnLoopCounter));
+                newCellsRow.Add(new Cell(newCellState));
+            }
+
+            return newCellsRow;
+        }
+
+        private CellState CreateNewCell(CellPosition cellPosition)
+        {
+            var newCellState = CellState.Dead;
+            var numberOfLiveNeighboursForCell = _board.GetNumberOfLiveNeighboursForCell(new CellPosition(cellPosition.Row, cellPosition.Column));
+
+            // Matt King 14/10/2022 - See if we can rmove this at the end and rely on a default newCellState of Dead instead.
+            if (numberOfLiveNeighboursForCell.GetPopulationState() == PopulationState.UnderPopulated)
+            {
+                newCellState = CellState.Dead;
+            }
+
+            if (numberOfLiveNeighboursForCell.GetPopulationState() == PopulationState.PerfectlyPopulated)
+            {
+                // Matt King 14/10/2022 - This is a temporary workaround to enable us to continue to defer moving to using a loop.  Remove this!!
+                var currentLeftCentreCellState = _board.Rows[cellPosition.Row].Cells[cellPosition.Column].State;
+                newCellState = currentLeftCentreCellState;
+            }
+
+            return newCellState;
         }
     }
 }
